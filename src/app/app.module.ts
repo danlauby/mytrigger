cceaa20c307f3227bac437713cdde46c66eeb9f4
danlauby <dmlauby@gmail.com>
2017-06-10 13:27:32 -0700

    Create doctors and resources components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,33 @@ import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { ResourcesComponent } from './resources/resources.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
+
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    DoctorsComponent,
+    ResourcesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule, 
-    routing
+    HttpModule,
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,21 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MainPageComponent } from './main-page/main-page.component';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 const appRoutes: Routes = [
     {
         path: '',
-        component: MainPageComponent
+        component: MainPageComponent,
+    },
+    {
+        path: 'doctors',
+        component: DoctorsComponent,
+    },
+    {
+        path: 'resources',
+        component: ResourcesComponent,
       }
 ];
 
